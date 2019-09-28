714587a1f2fada95cfdbe04f1c2b55f349243720
Orkhan ALIYEV <338396@ogr.ktu.edu.tr>
2019-09-28 19:15:19 -0400

    main.py duzenlendi

--- main.py
+++ main.py
@@ -61,8 +61,8 @@ genderProto = "gender_deploy.prototxt"
 genderModel = "gender_net.caffemodel"
 
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)',
-           '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+ageList = ['(0-3)', '(4-7)', '(8-14)', '(15-20)',
+           '(21-35)', '(36-45)', '(46-56)', '(57-100)']
 genderList = ['Male', 'Female']
 
 # Load network
